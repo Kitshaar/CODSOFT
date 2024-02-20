2192b5239e42d12d412f3027be5de283a19a0a71
Kartik Sharma <sharmakartik2322002@gmail.com>
2024-02-20 18:21:41 +0530

    Created getIntInput func

--- simpleCalculator/inputHandler.cpp
+++ simpleCalculator/inputHandler.cpp
@@ -42,3 +42,43 @@ double InputHandler::getDoubleInput(std::string_view prompt)
         }
     }
 }
+
+// Function to get a integer input from the user with the provided prompt
+int InputHandler::getIntInput(std::string_view prompt)
+{
+    while (true)
+    {
+        int value{};
+        // Display the prompt to the user
+        std::cout << prompt;
+        std::cin >> value;
+
+        // Check if the read operation failed
+        if (!std::cin)
+        {
+            // Check if the end of the file is reached
+            if (std::cin.eof())
+            {
+                exit(0); // Exit the program gracefully if end of file is reached
+            }
+
+            // Clear the fail state of std::cin
+            std::cin.clear();
+
+            // Ignore any remaining characters in the input buffer
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+            // Display an error message to the user
+            std::cout << "Invalid input. Please enter a valid number.\n";
+
+            continue;
+        }
+        else
+        {
+            // If input is valid, ignore any remaining characters in the input buffer
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            // Return the valid input value
+            return value;
+        }
+    }
+}
--- simpleCalculator/inputHandler.h
+++ simpleCalculator/inputHandler.h
@@ -8,6 +8,7 @@ class InputHandler
 public:
     // Function to get a double input from the user with the provided prompt
     static double getDoubleInput(std::string_view prompt);
+    static int InputHandler::getIntInput(std::string_view prompt);
 };
 
 #endif
